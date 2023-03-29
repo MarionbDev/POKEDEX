3b0fe2222d38a6709ad0e8f52e10bde8cbff6e4b
MarionbDev <marionbaston84@gmail.com>
2023-03-29 12:21:36 +0200

    ajout des props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import MyImage from './components/PokemonCard'
+import { useState } from "react";
+import "./App.css";
+import MyImage from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemon = pokemonList[0];
 
 function App() {
   return (
     <div>
-        <MyImage/>
+      <MyImage pokemon={pokemon} />
     </div>
   );
 }
