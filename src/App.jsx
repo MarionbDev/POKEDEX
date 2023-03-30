52694d60dacfdf02049589a876ce1f5272dadfe2
MarionbDev <marionbaston84@gmail.com>
2023-03-30 10:50:05 +0200

    ajout de state

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,46 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
-const pokemon = pokemonList[0];
+// const pokemon = pokemonList[0];
 
 function App() {
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+  const PrecedentHandeleClick = () => {
+    setpokemonIndex((precedentState) => precedentState - 1);
+  };
+
+  const SuivantHandeleClick = () => {
+    setpokemonIndex((suivantState) => suivantState + 1);
+  };
+
   return (
     <div>
-      <MyImage pokemon={pokemon} />
+      <MyImage pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 && (
+        <button onClick={PrecedentHandeleClick}>Précédent</button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={SuivantHandeleClick}>Suivant</button>
+      )}
     </div>
   );
 }
